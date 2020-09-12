ae39514691660a2edce484805012c57f92173c9b
Ankit <ankit.velani@gmail.com>
2020-09-12 22:32:05 +0530

    ++

---
+++ web/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+ <title>Indideli | Your One-Click Supermarket</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+  <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2&display=swap" rel="stylesheet">
+
+
+  <!-- Custom styles for this template -->
+  <link href="css/coming-soon.css" rel="stylesheet">
+  <style type="text/css">
+    
+    .overlay {
+    position: absolute;
+    top: 0;
+    left: 30%;
+    height: 100%;
+    width: 70%;
+    background-color: #f6ba00;
+    opacity: .99; 
+
+    z-index: 1;
+    background: url('./img/img_bg_1.jpg');
+    background-size:1100px 760px;
+    background-repeat: no-repeat;
+}
+
+.tagline{
+
+ font-size: 24px;font-family: 'Bad Script', cursive; font-weight: lighter;
+ text-align: right;
+}
+
+.logo{
+   font-family: 'Dosis', sans-serif;
+   font-size: 120px;
+   font-weight: bold;
+   color:white;
+}
+
+.submit{
+background-color: #FCC52A;border:1px solid white;
+color:white;
+ 
+
+}
+ .submit:hover{
+
+background-color: #f59301;border:2px solid white;
+color:white;
+ 
+}
+.social-icons ul > li > a {
+    display: block;
+    color: white;
+    background-color: #FCC52A;
+    border-radius: 100%;
+    font-size: 1.5rem;
+    line-height: 3rem;
+    height: 3rem;
+    width: 3rem;
+   
+}
+
+.social-icons ul > li > a:hover {
+    display: block;
+    color: white;
+    background-color: #f59301;
+    border:2px solid white;
+    border-radius: 100%;
+    font-size: 2rem;
+    line-height: 4rem;
+    height: 4rem;
+    width: 4rem;
+
+}
+
+.logo-section{
+margin-top: -100px;
+
+}
+@media (max-width: 600px) {
+   
+    .overlay {
+    position: absolute;
+    top: 45%;
+    left: 0;
+    height: 55%;
+    width: 100%;
+    background-color: white;
+    opacity: .99; 
+
+    z-index: 1;
+    background: url(./img/img_bg_1.jpg);;
+    background-size:500px 520px;
+    background-repeat: no-repeat;
+    }
+
+.tagline{
+  text-align: center;
+}
+.logo-section{
+  margin-top: -10px;
+}
+}
+
+  </style>
+ 
+</head>
+
+<body style="background: url('./img/img_bg_1.jpg');">
+
+  <div class="overlay"></div>
+ 
+
+  <div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto">
+          <div class="masthead-content  text-white py-5 py-md-0 logo-section" style="">
+            <img src="./img/logo.jpg" class="mx-auto d-block" style=";max-width: 340px;"/>
+<p  class="tagline">  your one-click supermarket</p>
+             
+ <br> <br><br>
+            <form class="center-block" style="margin-left: 10px;">
+                <p class="mb-5" style="margin-bottom: 2rem!important;font-size: 1.0rem!important;"> We are coming soon.<br>
+Get notified by email when it's ready!</p>
+            <div class="input-group input-group-newsletter">
+              <input type="email" class="form-control" placeholder="Get notified by email"  aria-describedby="submit-button" style="font-size:12px;border:1px solid white;">
+              <div class="input-group-append">
+                <button type='submit' class="btn submit" type="button" id="submit-button" style="font-size: 10px;font-weight: bold;">Notify Me!</button>
+              </div>
+            </div>
+          </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="social-icons">
+    <ul class="list-unstyled text-center mb-0">
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+ 
+
+</body>
+
+</html>
